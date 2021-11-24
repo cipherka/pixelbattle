4cec17d9c642eed7d8a9a67065440668f02fae30
cipherka <32297717+vlfz@users.noreply.github.com>
2021-11-24 23:09:04 +0500

    Update updatePixel.js

--- src/routes/updatePixel.js
+++ src/routes/updatePixel.js
@@ -3,36 +3,6 @@ const rethinkdb = require('rethinkdb');
 
 const hexRegExp = /^#[0-9A-F]{6}$/i;
 
-/* const net = require('net');
-const whiteList = new net.BlockList();
-whiteList.addRange('5.79.128.0', '5.79.255.255');
-whiteList.addRange('5.206.0.0', '5.206.127.255');
-whiteList.addRange('31.207.128.0', '31.207.255.255');
-whiteList.addRange('37.140.0.0', '37.140.127.255');
-whiteList.addRange('77.222.96.0', '77.222.127.255');
-whiteList.addRange('78.29.0.0', '78.29.15.255');
-whiteList.addRange('78.29.16.0', '78.29.23.255');
-whiteList.addRange('78.29.24.0', '78.29.27.255');
-whiteList.addRange('78.29.28.0', '78.29.29.255');
-whiteList.addRange('78.29.30.0', '78.29.30.255');
-whiteList.addRange('78.29.32.0', '78.29.63.255');
-whiteList.addRange('80.255.80.0', '80.255.95.255');
-whiteList.addRange('83.142.160.0', '83.142.167.255');
-whiteList.addRange('88.206.0.0', '88.206.127.255');
-whiteList.addRange('94.24.129.0', '94.24.129.255');
-whiteList.addRange('94.24.130.0', '94.24.131.255');
-whiteList.addRange('94.24.132.0', '94.24.135.255');
-whiteList.addRange('94.24.136.0', '94.24.143.255');
-whiteList.addRange('94.24.144.0', '94.24.159.255');
-whiteList.addRange('94.24.160.0', '94.24.191.255');
-whiteList.addRange('94.24.192.0', '94.24.255.255');
-whiteList.addRange('109.191.0.0', '109.191.255.255');
-whiteList.addRange('176.226.128.0', '176.226.255.255');
-whiteList.addRange('185.12.228.0', '185.12.231.255');
-whiteList.addRange('193.33.26.0', '193.33.27.255');
-whiteList.addRange('193.105.156.0', '193.105.156.255');
-whiteList.addRange('195.114.122.0', '195.114.123.255'); */
-
 module.exports = (r) => ({
     method: "POST",
     url: '/pixels/put',
@@ -78,11 +48,10 @@ module.exports = (r) => ({
         if (!hexRegExp.test(color)) return res.send({ error: true, reason: "IncorrectColor" });
 
         const pixel = rethinkdb.db('pixelbattle').table('pixels').get(pixelID).run(r);
-        if (!pixel) return res.send({ error: true, reason: "IncorrectPixel" });
+        if (!pixel || pixelID >= 861 || pixelID <= 4640) return res.send({ error: true, reason: "IncorrectPixel" });
 
         let cooldown;
-        let ip = req.headers['cf-connecting-ip'] || req.ip;
-        let adminCheck = false; // whiteList.check(ip);
+        let adminCheck = req.userSession.userID === "178404926869733376";
         switch (adminCheck) {
             case true:
                 cooldown = 0;
